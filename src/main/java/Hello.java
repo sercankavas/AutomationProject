056138ff916b9c409344df3f14403297d58eb647
Sercan Kavas <srsr141986@gmail.com>
2020-10-02 11:09:35 +0200

    EU3 doing great

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -4,5 +4,8 @@ public class Hello {
         System.out.println("Hello class EU3");
         //more changes are comming then
         //i am ready to commit this
+
+        //EU3 is so so
+        //EU is sth
     }
 }
